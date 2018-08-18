d4c565e2369083374a94ee5a303742597d59f8a1
Banner244 <42498260+Banner244@users.noreply.github.com>
2018-08-18 19:34:32 +0200

    Telefonmanager

---
+++ mysqltelefonbuch/mysqltelefonbuch/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace mysqltelefonbuch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNachname = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnVorname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnNachname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnNummer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVorname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelNummer = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Auflisten";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNachname
+            // 
+            this.txtNachname.Location = new System.Drawing.Point(9, 35);
+            this.txtNachname.Name = "txtNachname";
+            this.txtNachname.Size = new System.Drawing.Size(100, 20);
+            this.txtNachname.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnID,
+            this.columnVorname,
+            this.columnNachname,
+            this.columnNummer});
+            this.listView1.Location = new System.Drawing.Point(6, 48);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(417, 157);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnID
+            // 
+            this.columnID.Text = "ID";
+            // 
+            // columnVorname
+            // 
+            this.columnVorname.Text = "Vorname";
+            this.columnVorname.Width = 88;
+            // 
+            // columnNachname
+            // 
+            this.columnNachname.Text = "Nachname";
+            this.columnNachname.Width = 114;
+            // 
+            // columnNummer
+            // 
+            this.columnNummer.Text = "Nummer";
+            this.columnNummer.Width = 148;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Löschen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 161);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(429, 211);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Daten";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtTelNummer);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtVorname);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.txtNachname);
+            this.groupBox2.Location = new System.Drawing.Point(3, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(429, 143);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Neue Kontakte";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(272, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 120);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Hinzufügen";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nachname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Vorname";
+            // 
+            // txtVorname
+            // 
+            this.txtVorname.Location = new System.Drawing.Point(9, 76);
+            this.txtVorname.Name = "txtVorname";
+            this.txtVorname.Size = new System.Drawing.Size(100, 20);
+            this.txtVorname.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nummer";
+            // 
+            // txtTelNummer
+            // 
+            this.txtTelNummer.Location = new System.Drawing.Point(9, 116);
+            this.txtTelNummer.Name = "txtTelNummer";
+            this.txtTelNummer.Size = new System.Drawing.Size(100, 20);
+            this.txtTelNummer.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 375);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNachname;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnID;
+        private System.Windows.Forms.ColumnHeader columnVorname;
+        private System.Windows.Forms.ColumnHeader columnNachname;
+        private System.Windows.Forms.ColumnHeader columnNummer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelNummer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVorname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ mysqltelefonbuch/mysqltelefonbuch/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace mysqltelefonbuch
+{
+    public partial class Form1 : Form
+    {
+        string myConnectionString = "SERVER=127.0.0.1;" +
+                    "DATABASE=db;" +
+                    "UID=db;" +
+                    "PASSWORD=pass;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM daten";
+            MySqlDataReader Reader;
+            connection.Open();
+            Reader = command.ExecuteReader();
+            while (Reader.Read())
+            {
+                string row = "";
+                
+                for (int i = 0; i < Reader.FieldCount; i++)
+                {
+              //      row = Reader.GetValue(i).ToString()/* + ", "*/;
+                    if(i==Reader.FieldCount-1)
+                    {
+                        ListViewItem lvl = new ListViewItem(Reader.GetValue(0).ToString());
+                        lvl.SubItems.Add(Reader.GetValue(1).ToString());
+                        lvl.SubItems.Add(Reader.GetValue(2).ToString());
+                        lvl.SubItems.Add(Reader.GetValue(3).ToString());
+                        listView1.Items.Add(lvl);
+                    }
+                    //  listView1.Items[0].SubItems[0].Text = "";
+                }
+                
+                //    textBox1.Text+= row;
+
+
+            }
+            connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlCommand command = connection.CreateCommand();
+
+            int loeschindex = int.Parse( listView1.SelectedItems[0].Text);
+            command.CommandText = "DELETE FROM daten " +
+                                  "WHERE ID =" + loeschindex + " " +
+                                  "limit 1";
+            //    "Values('dfdf')";
+
+            command.Connection = connection;
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlCommand command = connection.CreateCommand();
+
+            command.CommandText = "INSERT INTO daten VALUES('', '"+txtNachname.Text+"', '"+txtVorname.Text+"', '"+txtTelNummer.Text+"')";
+            //    "Values('dfdf')";
+
+            command.Connection = connection;
+            connection.Open();
+            command.ExecuteNonQuery();
+        
+            connection.Close();
+        }
+    }
+}
